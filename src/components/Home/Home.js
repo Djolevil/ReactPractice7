23e19afb65cf3eba38535327fee67f28f402ca3a
Djordje Knezevic <starwarrior@mycity.rs>
2022-03-27 19:55:57 +0200

    Add logout button to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
